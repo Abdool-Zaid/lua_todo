4f928e5945ee2737e4530a96c68b372954a59876
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-08-15 18:23:30 +0200

    'loop_complete'

--- main.lua
+++ main.lua
@@ -1,20 +1,35 @@
- local file = io.open("data.tsk", "r")
-local content=  file:read("*all")
-file:close()
---  print(content)
+ local ops = require("ops")
+  
+
 function input_command()
     io.write("commandlist: \nd:display tasks\nu:update tasks\nn:new task\nr:remove task\nselected: ")
     return io.read()
 end
+
+function loop_func()
 local res = input_command()
-if res == "d" then
-
-elseif res == "u" then
+ 
+    if res == "d" then
+        ops.read()
     
-elseif res == "n" then
-
-elseif res == "r" then   
-
-else
--- failure state
-end    
+    elseif res == "u" then
+        ops.update()
+    
+    elseif res == "n" then
+        ops.create()
+    
+    elseif res == "r" then   
+        ops.delete()
+    
+    else
+    -- failure state
+    end    
+end   
+local iter=false
+while true do 
+    if iter==true then
+    print("\n\n\n")
+    end
+        loop_func()
+        iter=true
+end
--- ops.lua
+++ ops.lua
@@ -1,16 +1,23 @@
-function ceate()
+local ops = {}
 
+
+function ops.ceate()
+    print("create new todo")
 end
 
-function read()
-
+function ops.read()
+    local file = io.open("data.tsk", "r")
+    local content=  file:read("*all")
+    file:close()
+    print(content)
 end
 
-function update()
-
+function ops.update()
+    print("update todo")
 end
 
-function delete()
-
+function ops.delete()
+    print("delete todo")
 end
 
+return ops
